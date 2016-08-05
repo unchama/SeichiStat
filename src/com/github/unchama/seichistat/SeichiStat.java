d66f4cde6883b30cc1f46d7ca40a5b9530bfd544
unchama <nintendowii@jcom.home.ne.jp>
2016-08-06 06:38:18 +0900

    birth

---
+++ src/com/github/unchama/seichistat/SeichiStat.java
@@ -0,0 +1,14 @@
+package com.github.unchama.seichistat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SeichiStat  extends JavaPlugin {
+	@Override
+	public void onEnable(){
+
+	}
+	@Override
+	public void onDisable(){
+
+	}
+}
