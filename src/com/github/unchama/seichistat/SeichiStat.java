f60d39648b5cf946d558dafff51ee8156c6e5b02
unchama <nintendowii@jcom.home.ne.jp>
2016-08-19 11:19:58 +0900

    update

--- src/com/github/unchama/seichistat/SeichiStat.java
+++ src/com/github/unchama/seichistat/SeichiStat.java
@@ -3,9 +3,14 @@ package com.github.unchama.seichistat;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SeichiStat  extends JavaPlugin {
+	
+	public static SeichiStat plugin;
+	
+	
 	@Override
 	public void onEnable(){
-
+		plugin = this;
+		
 	}
 	@Override
 	public void onDisable(){
